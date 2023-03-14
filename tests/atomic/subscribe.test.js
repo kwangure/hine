704efd56935e2bcaea3db21f424ace4240b14918
kwangure <kaf.wangure@gmail.com>
2023-03-14 11:03:43 +0300

    test: subscribers are called on dispatch

--- tests/atomic/subscribe.test.js
+++ tests/atomic/subscribe.test.js
@@ -33,12 +33,35 @@ describe('subscribe', () => {
 			actions: {},
 			always: [],
 			entry: [],
-		});
+		}).resolve();
 		let count = 0;
 		machine.subscribe(() => count++);
 		expect(count).toBe(1);
 
-		machine.resolve().start();
+		machine.start();
 		expect(count).toBe(2);
+	});
+	it('calls subscribers on disptach', () => {
+		const machine = new AtomicState({
+			actions: {
+				noop() {},
+			},
+			on: {
+				event: [{
+					actions: ['noop'],
+				}],
+			},
+		})
+			.resolve()
+			.start();
+		let count = 0;
+		machine.subscribe(() => count++);
+		expect(count).toBe(1);
+
+		machine.dispatch('event');
+		expect(count).toBe(2);
+
+		machine.dispatch('useless');
+		expect(count).toBe(3);
 	});
 });

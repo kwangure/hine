abe264c990e7ec2784f745121d315023af7e49ac
kwangure <kaf.wangure@gmail.com>
2023-05-09 21:42:48 +0300

    feat: require non-null state for activePath

--- src/utils/state.js
+++ src/utils/state.js
@@ -2,13 +2,17 @@ import { CompoundState } from '../compound.js';
 
 /**
  * Returns the active path of a state
- * @param {import('../types.js').StateNode | null} state
+ * @param {import('../types.js').StateNode} state
  * @returns {string}
  */
 export function activePath(state) {
 	if (state instanceof CompoundState) {
-		return `${state.name}.${activePath(state.state)}`;
+		if (state.state) {
+			return state.state
+				? `${state.name}.${activePath(state.state)}`
+				: state.name;
+		}
 	}
 
-	return state?.name || '';
+	return state.name || '';
 }

6aeb0156dfa5627db4e208c606c19888d955f349
kwangure <kaf.wangure@gmail.com>
2023-06-15 00:33:38 +0300

    refactor: make lint root-level

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,20 +3,4 @@ module.exports = {
 	extends: [
 		'@kwangure/eslint-config-svelte',
 	],
-	settings: {
-		'import/resolver': {
-			alias: {
-				map: [
-					// escape `$` to work around eslint's Regex matching
-					['\\$app', './playground/node_modules/@sveltejs/kit/src/runtime/app/'],
-					['\\$lib', './playground/src/lib/'],
-					['\\$eventscript', './src/'],
-				],
-				extensions: ['.js', '.svelte', '.json'],
-			},
-		},
-	},
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
 };

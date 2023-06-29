220793f7d2167370786fa3e951c0e69022aeef07
kwangure <kaf.wangure@gmail.com>
2023-06-29 17:27:29 +0300

    feat: compile to config directly for now

--- packages/hine-next/src/compiler/compile.js
+++ packages/hine-next/src/compiler/compile.js
@@ -2,31 +2,37 @@ import { simple } from './ast.js';
 
 function OnElement(node, onElements) {
 	if (node.name === 'on') {
-		const events = [];
-		simple(node, { Element: EventElement }, events);
-		onElements.push({ events });
+		simple(node, { Element: EventElement }, onElements);
 	}
 }
 
-function EventElement(node, eventElements) {
+function EventElement(node, onElements) {
 	if (node.name === 'event') {
-		const event = { name: '', actions: [] };
-		simple(node, { Attribute: ActionOrNameAttribute }, event);
-		eventElements.push(event);
+		const handler = {};
+		simple(node, { Attribute: ActionOrNameAttribute }, handler);
+		let handlers = onElements[handler.name];
+		if (!handlers) {
+			handlers = [];
+			onElements[handler.name] = handlers;
+		}
+		handlers.push({ actions: handler.actions });
 	}
 }
 
 function ActionOrNameAttribute(node, eventElement) {
 	if (node.name === 'name') {
-		eventElement.name = node.value.raw;
+		eventElement.name = node.value[0].raw;
 	}
 	if (node.name === 'action') {
+		if (!Array.isArray(eventElement.actions)) {
+			eventElement.actions = [];
+		}
 		eventElement.actions.push(node.value[0].raw);
 	}
 }
 
 export function compile(ast) {
-	const onNodes = [];
-	simple(ast, { Element: OnElement }, onNodes);
-	return { on: onNodes };
+	const onNode = {};
+	simple(ast, { Element: OnElement }, onNode);
+	return { on: onNode };
 }

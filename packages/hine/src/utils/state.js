45fee98a7c95ba5039d721e46f18d8f643f72f6c
kwangure <kaf.wangure@gmail.com>
2023-08-01 00:09:58 +0300

    feat: remove stateEventNames

--- packages/hine/src/utils/state.js
+++ packages/hine/src/utils/state.js
@@ -12,21 +12,3 @@ export function activePath(state) {
 
 	return state.name;
 }
-
-/**
- * @param {import("../types.js").StateNode} state
- * @param {string[]} [stateTreeEvents]
- */
-export function stateEventNames(state, stateTreeEvents = []) {
-	for (const name of Object.keys(state.__onHandler)) {
-		if (!stateTreeEvents.includes(name)) {
-			stateTreeEvents.push(name);
-		}
-	}
-
-	if (state instanceof CompoundState && state.__state) {
-		stateEventNames(state.__state, stateTreeEvents);
-	}
-
-	return stateTreeEvents;
-}

5f678b1898fb2b1c123a1ba52ddb43524c476748
kwangure <kaf.wangure@gmail.com>
2023-08-09 22:27:01 +0300

    feat: improve primary sidebar aria

--- packages/svelte-ui/src/lib/components/sidebar/primary.svelte
+++ packages/svelte-ui/src/lib/components/sidebar/primary.svelte
@@ -2,8 +2,6 @@
 	class="sticky top-[var(--svui-navbar-height)] col-start-[nav-start] col-end-[content-start] row-start-2 row-end-3 max-h-screen w-60 self-start overflow-y-auto"
 >
 	<nav class="flex flex-col overflow-y-auto overscroll-contain px-2">
-		<ul>
-			<slot />
-		</ul>
+		<slot />
 	</nav>
 </aside>
--- packages/svelte-ui/src/lib/components/sidebar/section.svelte
+++ packages/svelte-ui/src/lib/components/sidebar/section.svelte
@@ -3,11 +3,11 @@
 	export let title;
 </script>
 
-<li class="flex flex-col gap-1">
-	<h5
-		class="sticky top-0 whitespace-nowrap bg-white pb-1 pt-2 text-xs font-semibold uppercase text-neutral-900 dark:bg-neutral-800 dark:text-neutral-400"
-	>
-		{title}
-	</h5>
+<h5
+	class="sticky top-0 whitespace-nowrap bg-white pb-1 pt-2 text-xs font-semibold uppercase text-neutral-900 dark:bg-neutral-800 dark:text-neutral-400"
+>
+	{title}
+</h5>
+<ul class="flex flex-col gap-1">
 	<slot />
-</li>
+</ul>

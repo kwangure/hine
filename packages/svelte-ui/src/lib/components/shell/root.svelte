bd226c9c1093b841207259be88820d908842dc8c
kwangure <kaf.wangure@gmail.com>
2023-08-10 02:06:51 +0300

    fix: account for row gap on sticky content

--- packages/svelte-ui/src/lib/components/shell/root.svelte
+++ packages/svelte-ui/src/lib/components/shell/root.svelte
@@ -1,5 +1,5 @@
 <div
-	class="grid min-h-screen grid-cols-[[nav-start]_min-content_[content-start]_1fr_[content-end]_min-content_[nav-end]] grid-rows-[var(--svui-navbar-height)_1fr] gap-y-2"
+	class="grid min-h-screen grid-cols-[[nav-start]_min-content_[content-start]_1fr_[content-end]_min-content_[nav-end]] grid-rows-[var(--svui-navbar-height)_1fr] gap-y-[var(--svui-navbar-y-gap)]"
 >
 	<slot />
 </div>
@@ -7,6 +7,7 @@
 <style>
 	:root {
 		--svui-navbar-height: 4rem;
+		--svui-navbar-y-gap: 0.5rem;
 		--svui-sidebar-width: 15rem;
 	}
 </style>

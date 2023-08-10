8ef5f2bd74357777491dad99cc010f107a90bfba
kwangure <kaf.wangure@gmail.com>
2023-08-10 23:44:52 +0300

    feat: update on this page after navigate and hash change

--- sites/hine.dev/src/lib/components/outline/outline.svelte
+++ sites/hine.dev/src/lib/components/outline/outline.svelte
@@ -1,7 +1,9 @@
 <script>
+	import { afterNavigate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { isPartiallyHidden } from '$lib/dom/dom.js';
 	import List from './list.svelte';
+	import { page } from '$app/stores';
 
 	/** @type {import('mdast').RootData['tableOfContents']} */
 	export let toc;
@@ -20,20 +22,36 @@
 		}
 	}
 
-	onMount(() => {
-		document.fonts.ready.then(() => {
-			// Initialize the active link
-			updateActiveSlug();
-			// Attach the scroll event listener to the main content
-			window.addEventListener('scroll', updateActiveSlug);
-		});
+	afterNavigate(updateActiveSlug);
 
-		return () => {
-			// Remove the event listener when the component is destroyed
-			window.removeEventListener('scroll', updateActiveSlug);
-		};
+	onMount(async () => {
+		await document.fonts.ready;
+
+		updateActiveSlug();
 	});
+
+	/**
+	 * Prioritize the changed hash over the output of the algorithm in
+	 * `updateActiveSlug()`
+	 *
+	 * @param {URL} url
+	 * @link {https://github.com/sveltejs/kit/blob/a6fe5fcb1c7258281b4bf53b94543272e6e6c6d8/sites/kit.svelte.dev/src/routes/docs/%5Bslug%5D/OnThisPage.svelte#L67}
+	 */
+	function preferHashchangeTarget(url) {
+		const update = () => (activeTarget = url.hash);
+
+		// belt...
+		queueMicrotask(update);
+
+		// ...and braces
+		addEventListener('scroll', update, { once: true });
+	}
 </script>
+
+<svelte:window
+	on:scroll={updateActiveSlug}
+	on:hashchange={() => preferHashchangeTarget($page.url)}
+/>
 
 <h5
 	class="whitespace-nowrap bg-white pb-1 pt-2 text-xs font-semibold uppercase text-neutral-900 dark:bg-neutral-800 dark:text-neutral-400"

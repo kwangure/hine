81ef4a4e6faab7a34bb888e95714beb7a8b446a0
kwangure <kaf.wangure@gmail.com>
2023-08-10 03:19:42 +0300

    feat: add grid margins

--- packages/svelte-ui/src/lib/components/shell/navbar.svelte
+++ packages/svelte-ui/src/lib/components/shell/navbar.svelte
@@ -1,5 +1,8 @@
 <nav
-	class="sticky top-0 z-40 col-start-[nav-start] col-end-[nav-end] row-start-1 row-end-2 flex items-center border-b border-neutral-600 bg-white dark:bg-neutral-800"
+	class="sticky top-0 z-40 col-start-[nav-start] col-end-[nav-end] row-start-1 row-end-2 flex items-center bg-white dark:bg-neutral-800"
 >
 	<slot />
 </nav>
+<div
+	class="sticky top-[var(--svui-navbar-height)] z-40 col-start-[page-start] col-end-[page-end] row-start-1 row-end-2 self-end border-b border-neutral-600"
+/>
--- packages/svelte-ui/src/lib/components/sidebar/secondary.svelte
+++ packages/svelte-ui/src/lib/components/sidebar/secondary.svelte
@@ -1,5 +1,5 @@
 <aside
-	class="sticky top-[calc(var(--svui-navbar-height)+var(--svui-navbar-y-gap))] col-start-[content-end] col-end-[nav-end] row-start-2 row-end-3 max-h-screen w-60 self-start overflow-y-auto"
+	class="sticky top-[calc(var(--svui-navbar-height)+var(--svui-navbar-y-gap))] col-start-[content-end] col-end-[nav-end] row-start-2 row-end-3 hidden max-h-screen w-60 self-start overflow-y-auto lg:block"
 >
 	<nav class="flex flex-col overflow-y-auto overscroll-contain px-2">
 		<slot />

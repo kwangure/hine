cdb4c9550ddeb1b88bba8314dddd2cf5fdd76931
kwangure <kaf.wangure@gmail.com>
2023-08-22 23:23:42 +0300

    feat: add darkmode toggle

--- sites/hine.dev/src/routes/docs/[slug]/+page.svelte
+++ sites/hine.dev/src/routes/docs/[slug]/+page.svelte
@@ -1,9 +1,27 @@
 <script>
 	import { Icon, Markdown, Shell, Sidebar } from '@hinejs/svelte-ui/components';
+	import { mdiWeatherNight, mdiWhiteBalanceSunny } from '@mdi/js';
 	import { page } from '$app/stores';
 	import { siGithub, siNpm } from 'simple-icons';
+	import { createDarkModeButton } from '@hinejs/svelte-ui/creators';
 
 	export let data;
+
+	const darkMode = createDarkModeButton();
+	const { button } = darkMode.elements;
+	const { mode } = darkMode.state;
+
+	let darkModeIcon = '';
+	let darkModeLabel = '';
+	$: {
+		if ($mode === 'dark') {
+			darkModeIcon = mdiWhiteBalanceSunny;
+			darkModeLabel =  'Switch to light mode';
+		} else if ($mode === 'light') {
+			darkModeIcon = mdiWeatherNight;
+			darkModeLabel  = 'Switch to dark mode';
+		}
+	}
 </script>
 
 <Shell.Root>
@@ -12,6 +30,7 @@
 		<div class="ml-auto flex gap-1 lg:pr-6">
 			<Icon.Link href="https://github.com/kwangure/hine" label="GitHub" path={siGithub.path}/>
 			<Icon.Link href="https://www.npmjs.com/package/hine" label="NPM" path={siNpm.path}/>
+			<Icon.Button use={button} label={darkModeLabel} path={darkModeIcon}/>
 		</div>
 	</Shell.Navbar>
 	<Sidebar.Root>

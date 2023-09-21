e35cb68c71d4d617002ecc4b8e14253d97904d76
kwangure <kaf.wangure@gmail.com>
2023-09-21 16:05:08 +0300

    test: check atomic state context types

--- packages/hine/tests/helpers.test.js
+++ packages/hine/tests/helpers.test.js
@@ -1,8 +1,66 @@
 import { expect, expectTypeOf, test } from 'vitest';
 import { handler, state } from '../src/helpers.js';
+import { AtomicState } from '../src/state/atomic.js';
+import { CompoundState } from '../src/state/compound.js';
+
+test('atomic state respects context types in actions and conditions', () => {
+	const stateMachine = state({
+		name: 'child0',
+		context: {
+			key0: String,
+			willChangeType: String,
+		},
+		entry: [handler({ if: 'condition0', run: ['action0'] })],
+	});
+
+	expectTypeOf(stateMachine).toMatchTypeOf(
+		/** @type {AtomicState<any, any>} */ (new AtomicState()),
+	);
+
+	stateMachine.resolve({
+		context: {
+			key0: '0',
+			willChangeType: 'foo',
+		},
+		actions: {
+			action0({ ownerState }) {
+				const context = ownerState.context;
+
+				const key0 = context.get('key0');
+				expectTypeOf(key0).toBeString();
+				expect(key0).toBe('0');
+
+				const willChangeType = context.get('willChangeType');
+				expectTypeOf(willChangeType).toBeString();
+				expect(willChangeType).toBe('foo');
+
+				// @ts-expect-error
+				context.get('non-existent');
+			},
+		},
+		conditions: {
+			condition0({ ownerState }) {
+				const context = ownerState.context;
+
+				const key0 = context.get('key0');
+				expectTypeOf(key0).toBeString();
+				expect(key0).toBe('0');
+
+				const willChangeType = context.get('willChangeType');
+				expectTypeOf(willChangeType).toBeString();
+				expect(willChangeType).toBe('foo');
+
+				// @ts-expect-error
+				context.get('non-existent');
+
+				return true;
+			},
+		},
+	});
+});
 
 test('compound state respects context types in actions and conditions', () => {
-	const test01 = state({
+	const stateMachine = state({
 		name: 'child0',
 		context: {
 			key0: String,
@@ -20,7 +78,17 @@ test('compound state respects context types in actions and conditions', () => {
 		},
 	});
 
-	test01.resolve({
+	expectTypeOf(stateMachine).toMatchTypeOf(
+		/** @type {CompoundState<any, any>} */ (
+			new CompoundState({
+				children: {
+					s1: new AtomicState(),
+				},
+			})
+		),
+	);
+
+	stateMachine.resolve({
 		context: {
 			key0: '0',
 			willChangeType: 'foo',

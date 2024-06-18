58b2d54c0e0fb8e5c1a560be0b1a0aa28b23842e
kwangure <kaf.wangure@gmail.com>
2024-06-18 23:22:09 +0300

    feat: implement hasActiveListener

--- packages/hine/src/state/method/index.ts
+++ packages/hine/src/state/method/index.ts
@@ -1,3 +1,4 @@
 export { emitEvent } from './emit.js';
+export { hasActiveListener } from './hasActiveListener.js';
 export { matches } from './matches.js';
 export { resolveState } from './resolve.js';

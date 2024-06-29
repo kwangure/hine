74315d0f89e1ca82a7f65ec9b6ecc8ecbfd5d969
kwangure <kaf.wangure@gmail.com>
2024-06-29 21:24:10 +0300

    feat: add `fromPromise` helper

---
+++ packages/hine/src/state/helper/fromPromise.ts
@@ -0,0 +1,92 @@
+import {
+	StateEvent,
+	atomic,
+	compound,
+	emitEvent,
+	normalizeListener,
+	type CompoundStateConfig,
+	type StateNode,
+} from 'hine';
+
+type OnHandler = NonNullable<CompoundStateConfig['on']>[string];
+
+export interface FromPromiseOptions {
+	hooks?: CompoundStateConfig['hooks'];
+	on?: {
+		reject?: OnHandler;
+		resolve?: OnHandler;
+	};
+}
+
+export function fromPromise(
+	name: string,
+	promise: Promise<unknown>,
+	options?: FromPromiseOptions,
+) {
+	let rootState: StateNode;
+	const isPromise = (event: StateEvent) =>
+		isPromiseStateEvent(event) && event.detail.promise === promise;
+	return compound(name, {
+		hooks: {
+			...options?.hooks,
+			afterEntry: [
+				(event) => {
+					rootState = event.target;
+					promise
+						.then((result) =>
+							emitEvent(rootState, 'resolve', {
+								result,
+								promise,
+							}),
+						)
+						.catch((result) =>
+							emitEvent(rootState, 'reject', { result, promise }),
+						);
+				},
+				...(options?.hooks?.afterEntry
+					? normalizeListener(options.hooks.afterEntry)
+					: []),
+			],
+		},
+		initial: 'pending',
+		children: [
+			atomic('pending', {
+				on: {
+					resolve: {
+						if: isPromise,
+						goto: 'resolved',
+					},
+					reject: {
+						if: isPromise,
+						goto: 'rejected',
+					},
+				},
+			}),
+			atomic('resolved'),
+			atomic('rejected'),
+		],
+		...(options?.on ? { on: options.on } : {}),
+	});
+}
+
+export interface PromiseStateEvent<
+	T extends 'reject' | 'resolve' = 'reject' | 'resolve',
+> extends StateEvent {
+	type: T;
+	detail: {
+		result: unknown;
+		promise: Promise<unknown>;
+	};
+}
+
+export function isPromiseStateEvent(
+	event: StateEvent,
+): event is PromiseStateEvent {
+	return (
+		(event.type === 'reject' || event.type == 'resolve') &&
+		typeof event.detail === 'object' &&
+		event.detail !== null &&
+		'promise' in event.detail &&
+		'result' in event.detail
+	);
+}
